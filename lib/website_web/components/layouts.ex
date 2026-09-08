d71b2bf709e5e0bfd07c8377183f0f641065a9e7
NikGabric <36707304+NikGabric@users.noreply.github.com>
2026-09-08 12:24:49 +0200

    chore: theme cleanup

--- lib/website_web/components/layouts.ex
+++ lib/website_web/components/layouts.ex
@@ -5,25 +5,10 @@ defmodule WebsiteWeb.Layouts do
   """
   use WebsiteWeb, :html
 
-  # Embed all files in layouts/* within this module.
-  # The default root.html.heex file contains the HTML
-  # skeleton of your application, namely HTML headers
-  # and other static content.
   embed_templates "layouts/*"
 
   @doc """
   Renders your app layout.
-
-  This function is typically invoked from every template,
-  and it often contains your application menu, sidebar,
-  or similar.
-
-  ## Examples
-
-      <Layouts.app flash={@flash}>
-        <h1>Content</h1>
-      </Layouts.app>
-
   """
   slot :inner_block, required: true
 
